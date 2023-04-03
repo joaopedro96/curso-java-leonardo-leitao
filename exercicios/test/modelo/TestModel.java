b6847d31d1173ab2122992bd07de7558f61f7f55
João Pedro da Mata Gonçalves Ribeiro <joaopedro.damata96@gmail.com>
2023-04-03 09:27:49 -0300

    adicao do folder de testes

---
+++ exercicios/test/modelo/TestModel.java
@@ -0,0 +1,15 @@
+package modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestModel {
+
+	@Test
+	void testarSeIgualADois() {
+		int a = 1 + 1;
+		assertEquals(a, 2);
+	}
+
+}
